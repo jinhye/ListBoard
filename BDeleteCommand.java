5334adb40ef4bcaf57b93b0359a5d9813e7ce620
jinhye <hanolanana8817@gmail.com>
2017-06-26 18:36:21 -0400

    Add files via upload

---
+++ BDeleteCommand.java
@@ -0,0 +1,18 @@
+package com.ourBoard.command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ourBoard.dao.BDao;
+
+public class BDeleteCommand implements BCommand {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) {
+		String bId = request.getParameter("bId");
+		BDao dao = new BDao();
+		dao.delete(bId);
+
+	}
+
+}
